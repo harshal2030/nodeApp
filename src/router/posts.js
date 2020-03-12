beb163eb196323a4fa79e22c4acde75c09f502db
harshal <harshal2030@outlook.com>
2020-03-13 00:54:35 +0530

    tryin to support video upload

--- src/router/posts.js
+++ src/router/posts.js
@@ -4,17 +4,20 @@ const Post = require('./../models/post');
 const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
-const uuidv4 = require('uuid/v4')
+const uuidv4 = require('uuid/v4');
+const fs = require('fs');
+const ffmpeg = require('fluent-ffmpeg');
 
 const router = express.Router();
 const postImgPath = path.join(__dirname, '../../public/images/posts')
+const postVideoPath = path.join(__dirname, './../../public/videos/posts')
 
 const upload = multer({
     limits: {
-        fileSize: 20 * 1000000
+        fileSize: 200 * 1000000
     },
     fileFilter(req, file, cb) {
-        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png|mp4|mkv)$/)) {
             return cb(Error("Unsupported files uploaded to the server"))
         }
 
@@ -22,19 +25,34 @@ const upload = multer({
     }
 })
 
-router.post('/posts', auth, upload.single('image'), async (req, res) => {
+const mediaMiddleware = upload.fields([
+    {name: 'image', maxCount: 1},
+    {name: 'video'}
+])
+
+router.post('/posts', auth, mediaMiddleware, async (req, res) => {
     try {
         post = JSON.parse(req.body.info);
         post.username = req.user.username;
         post['name'] = req.user.name;
-    
-        if (req.file !== undefined) {
+        
+        const file = req.files;
+        console.log(file.video);
+        if (file.image[0] !== undefined) {
             const filename = `${uuidv4()}.png`;
             const filePath = postImgPath + '/' + filename;
-            await sharp(req.file.buffer).png().toFile(filePath);
+            await sharp(file.image[0].buffer).png().toFile(filePath);
             post['mediaPath'] = filePath;
             post['mediaIncluded'] = true;    
+        } else if (file.video[0] !== undefined) {
+            stream = fs.createWriteStream('steam.divx')
+            const filename = `${uuidv4()}.mp4`
+            const filePath = postVideoPath + '/' +filename;
+            await ffmpeg(file.video[0].buffer).output(filePath).output(stream)
+            post['mediaPath'] = filePath;
+            post['mediaIncluded'] = true; 
         }
+
         console.log(post)
     
         await Post.create(post);
