0514791ba1cf2bd3cb9be6d71d03ddf65f80bc31
harshal <harshal2030@outlook.com>
2020-05-18 14:32:21 +0530

    setted up migrations

---
+++ src/migrations/20200518085427-bookmarks.js
@@ -0,0 +1,34 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable no-unused-vars */
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addConstraint('bookmarks', ['postId'], {
+      type: 'foreign key',
+      name: 'bookmarks_postId_fkey',
+      references: {
+        table: 'posts',
+        field: 'postId',
+      },
+      onDelete: 'cascade',
+      onUpdate: 'cascade',
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeConstraint('bookmarks', 'bookmarks_postId_fkey');
+  },
+};
