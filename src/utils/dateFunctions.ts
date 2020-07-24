76e7d62b5ca0d6c0cf2874959afde733717b644a
harshal <harshal2030@outlook.com>
2020-07-25 00:03:29 +0530

    migrated utils to TS

--- src/utils/dateFunctions.ts
+++ src/utils/dateFunctions.ts
@@ -1,15 +1,17 @@
+import moment from 'moment';
+import { date } from 'faker';
 /**
  * Get the most latest date from array of objects
  * @param {Array} dates array of objects which contain column "createdAt"
  * @returns {Date} latest date from array
  */
-const maxDate = (dates) => new Date(Math.max.apply(null, dates.map((e) => new Date(e.createdAt))));
+const maxDate = (dates: Array<{ createdAt: Date }>): Date => moment.max(dates.map((date) => moment(date.createdAt))).toDate();
 
 /**
  * Get the last date from array of objects
  * @param {Array} dates array of objects which contain column "createdAt"
  * @returns {Date} last date from array of objects
  */
-const minDate = (dates) => new Date(Math.min.apply(null, dates.map((e) => new Date(e.createdAt))));
+const minDate = (dates: Array<{ createdAt: Date }>): Date => moment.min(dates.map((date) => moment(date.createdAt))).toDate(); 
 
-module.exports = { maxDate, minDate };
+export { maxDate, minDate };
