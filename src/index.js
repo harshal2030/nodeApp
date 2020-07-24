3650b7f34356e4b531393a666eeeb2b1d1554512
Harshal <harshal2030@outlook.com>
2020-07-24 20:47:32 +0530

    server push

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ const server = require('./app');
 const port = process.env.PORT;
 
 server.listen(port, () => {
-  console.log('Server listening on ', +port);
+  console.log(`Server listening on ${port}`);
 });
